a312056c7e0c1136b8f6521161ad64a6b8c14584
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-03-16 16:18:19 -0400

    Add files via upload
    hospedando meu site

---
+++ main.js
@@ -0,0 +1,38 @@
+var canvas=document.getElementById("myCanvas");
+var contexto=canvas.getContext("2d");
+var cor1="blue";
+var cor2="orange";
+var cor3="black";
+var cor4="green";
+var cor5="red";
+var largura=2;
+
+contexto.beginPath();
+contexto.strokeStyle=cor1;
+contexto.lineWidth=largura;
+contexto.arc(300, 300, 45, 0, 360);
+contexto.stroke();
+
+contexto.beginPath();
+contexto.strokeStyle=cor2;
+contexto.lineWidth=largura;
+contexto.arc(350, 350, 45, 0, 360);
+contexto.stroke();
+
+contexto.beginPath();
+contexto.strokeStyle=cor3;
+contexto.lineWidth=largura;
+contexto.arc(402, 300, 45, 0, 360);
+contexto.stroke();
+
+contexto.beginPath();
+contexto.strokeStyle=cor4;
+contexto.lineWidth=largura;
+contexto.arc(454, 350, 45, 0, 360);
+contexto.stroke();
+
+contexto.beginPath();
+contexto.strokeStyle=cor5;
+contexto.lineWidth=largura;
+contexto.arc(508, 300, 45, 0, 360);
+contexto.stroke();
