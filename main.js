b633f7c664b8e9f693080eef8eb78039ec7064b6
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-03-17 16:08:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ var cor2="orange";
 var cor3="black";
 var cor4="green";
 var cor5="red";
+var cor6="gray"
 var largura=2;
+
+contexto.beginPath();
+contexto.strokeStyle=cor6;
+contexto.lineWidth=largura;
+contexto.rect(200, 200, 430, 300);
+contexto.stroke();
 
 contexto.beginPath();
 contexto.strokeStyle=cor1;
